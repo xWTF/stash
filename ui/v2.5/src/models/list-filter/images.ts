e86a8ce8da9676f30bdabcec0cdb48f705ef42ce
xWTF <e@x.wtf>
2023-06-29 20:58:57 +0800

    default filter tweak

--- ui/v2.5/src/models/list-filter/images.ts
+++ ui/v2.5/src/models/list-filter/images.ts
@@ -25,7 +25,7 @@ import { PhashCriterionOption } from "./criteria/phash";
 import { CustomFieldsCriterionOption } from "./criteria/custom-fields";
 import { FolderCriterionOption } from "./criteria/folder";
 
-const defaultSortBy = "path";
+const defaultSortBy = "title";
 
 const sortByOptions = [
   "filesize",
